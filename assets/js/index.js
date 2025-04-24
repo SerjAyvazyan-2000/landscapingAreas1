d31768f09f5615cea33503a984753c4b723884a8
SerjAyvazyan-2000 <ayvazyanserj3@gmail.com>
2025-04-24 07:53:11 +0300

    new

---
+++ assets/js/index.js
@@ -0,0 +1,141 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+const header = document.querySelector('header');
+const menuBg = document.querySelector('.menu-bg');
+const headerBody = document.querySelector('.header-body');
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    burger.addEventListener("click", function (e) {
+        if (menu.classList.contains('active')) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
+    });
+    menuBg.addEventListener("click", function (e) {
+        if (menu.classList.contains('active')) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
+    });
+    document.querySelectorAll('.menu-list li a').forEach(link => {
+        link.addEventListener("click", closeMenu);
+    });
+
+    function openMenu() {
+        menu.classList.add('active');
+        burger.classList.add('active');
+        header.classList.add('active');
+        menuBg.classList.add('active');
+        headerBody.classList.add('active');
+
+
+    }
+
+    function closeMenu() {
+        menu.classList.remove('active');
+        burger.classList.remove('active');
+        header.classList.remove('active');
+        menuBg.classList.remove('active');
+        headerBody.classList.remove('active');
+
+    }
+
+
+});
+
+window.addEventListener('scroll', function () {
+    const headerTop = document.querySelector('header')
+
+
+    if (window.scrollY > 0) {
+        headerTop.classList.add('moved');
+
+    } else {
+        headerTop.classList.remove('moved');
+
+    }
+});
+
+document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+        const faqItem = button.parentElement;
+        faqItem.classList.toggle('active');
+    });
+});
+
+document.querySelectorAll('.home-link ').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            const headerHeight = document.querySelector('.header').offsetHeight;
+            const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - headerHeight;
+
+            window.scrollTo({
+                top: targetPosition,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const swiperContainer = document.querySelector(".mySwiper");
+
+    if (swiperContainer) {
+        const swiper = new Swiper(".mySwiper", {
+            loop: true,
+            spaceBetween: 30,
+            centeredSlides: true,
+            autoplay: {
+                delay: 3500,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                640: {
+                    slidesPerView: 1.2,
+                },
+                768: {
+                    slidesPerView: 2,
+                },
+                1024: {
+                    slidesPerView: 2.5,
+                },
+                1280: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+    }
+});
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    if (!localStorage.getItem("cookiesAccepted")) {
+        document.getElementById("cookieNotice").style.display = "flex";
+
+        document.getElementById("acceptCookies").addEventListener("click", function() {
+            document.getElementById("cookieNotice").style.display = "none";
+            localStorage.setItem("cookiesAccepted", "true");
+        });
+    }
+});
